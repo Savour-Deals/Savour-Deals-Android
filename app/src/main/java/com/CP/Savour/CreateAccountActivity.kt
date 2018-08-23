fb53f04339ec6293211b637979616c1441718989
wowbob396 <wowbob396@gmail.com>
2018-08-22 20:21:33 -0500

    re-created register and login pages

---
+++ app/src/main/java/com/CP/Savour/CreateAccountActivity.kt
@@ -0,0 +1,12 @@
+package com.CP.Savour
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateAccountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+    }
+}
