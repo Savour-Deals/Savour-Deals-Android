d8bb627dff4543ac0e83b304dc04124e48a19ed3
wowbob396 <wowbob396@gmail.com>
2018-09-16 18:24:40 -0500

    Created constructor for vendor class

--- app/src/main/java/com/CP/Savour/Vendor.kt
+++ app/src/main/java/com/CP/Savour/Vendor.kt
@@ -6,18 +6,28 @@ package com.CP.Savour
  */
 class Vendor {
     var address: String? = null
+    var dailyHours: Map<String, Any>? = null
     var description: String? = null
-    var dailyHours: Map<String, Object>? = null
-    var followers: Map<String,Object>? = null
-    var loyalty: Map<String, Object>? = null
+    var followers: Map<String,Any>? = null
+    var loyalty: Map<String, Any>? = null
     var menu: String? = null
     var name: String? = null
     var phone: String? = null
     var photo: String? = null
     var placeId: String? = null
+    var subscriptionId: String? = null
 
-
-    constructor() {
-
+    constructor(address: String?, dailyHours: Map<String, Any>?, description: String?, followers: Map<String, Any>?, loyalty: Map<String, Any>?, menu: String?, name: String?, phone: String?, photo: String?, placeId: String?, subscriptionId: String?) {
+        this.address = address
+        this.dailyHours = dailyHours
+        this.description = description
+        this.followers = followers
+        this.loyalty = loyalty
+        this.menu = menu
+        this.name = name
+        this.phone = phone
+        this.photo = photo
+        this.placeId = placeId
+        this.subscriptionId = subscriptionId
     }
 }
