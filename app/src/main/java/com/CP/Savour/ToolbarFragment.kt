f6ec19c111beecc62e2d0597f09e3ce0a5d90f67
wowbob396 <wowbob396@gmail.com>
2018-10-02 18:40:09 -0500

    added basic toolbar fragment

---
+++ app/src/main/java/com/CP/Savour/ToolbarFragment.kt
@@ -0,0 +1,38 @@
+package com.CP.Savour
+
+import android.support.annotation.IdRes
+import android.support.annotation.MenuRes
+import android.support.annotation.StringRes
+import android.view.MenuItem
+
+class ToolbarFragment(@IdRes val resId: Int,
+                      @StringRes title: Int,
+                      @MenuRes menuId: Int,
+                      val menuItems: MutableList<Int>,
+                      val menuClicks: MutableList<MenuItem.OnMenuItemClickListener?>) {
+
+    companion object {
+        @JvmField val NO_TOOLBAR = -1
+    }
+
+    class Builder {
+        private var resId: Int = -1
+        private var menuId: Int = -1
+        private var title: Int = -1
+        private var menuItems: MutableList<Int> = mutableListOf()
+        private var menuClicks: MutableList<MenuItem.OnMenuItemClickListener?> = mutableListOf()
+
+        fun withId(@IdRes resId: Int) = apply { this.resId = resId }
+
+        fun withTitle(title: Int) = apply { this.title = title }
+
+        fun withMenu(@MenuRes menuId: Int) = apply { this.menuId = menuId}
+
+        fun withMenuItems(menuItems: MutableList<Int>, menuClicks: MutableList<MenuItem.OnMenuItemClickListener?>) = apply {
+            this.menuItems.addAll(menuItems)
+            this.menuClicks.addAll(menuClicks)
+        }
+
+        fun build() = ToolbarFragment(resId, title, menuId, menuItems, menuClicks )
+    }
+}
