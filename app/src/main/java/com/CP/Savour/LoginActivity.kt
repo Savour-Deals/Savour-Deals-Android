c287761d1921aca9cf28675aeec7fb0eb122b041
wowbob396 <wowbob396@gmail.com>
2018-10-01 19:05:20 -0500

    got facebook login going

--- app/src/main/java/com/CP/Savour/LoginActivity.kt
+++ app/src/main/java/com/CP/Savour/LoginActivity.kt
@@ -9,16 +9,22 @@ import android.view.View
 import android.widget.*
 import com.google.firebase.auth.FirebaseAuth
 import android.view.WindowManager
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.facebook.login.widget.LoginButton
 import com.squareup.picasso.Picasso
 import com.google.firebase.auth.FirebaseUser
-
-
+import java.util.*
 
 
 class LoginActivity : AppCompatActivity() {
 
     private val TAG = "LoginActivity"
-    
+    private final  val EMAIL = "email"
+
     private var email: String? = null
     private var password: String? = null
 
@@ -30,8 +36,8 @@ class LoginActivity : AppCompatActivity() {
     private var editTextEmail: EditText? = null
     private var editTextPassword: EditText? = null
     private var buttonLogin: Button? = null
-
-    private var constraintView: View? = null
+    private lateinit var facebookButton: LoginButton
+    private lateinit var callbackManager: CallbackManager
 
     // Firebase references
     private var mAuth: FirebaseAuth? = null
@@ -43,12 +49,40 @@ class LoginActivity : AppCompatActivity() {
         initialize()
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+    }
+
     private fun initialize() {
         textViewForgotPassword = findViewById(R.id.tv_forgot_password) as TextView
         textViewCreateAccount = findViewById(R.id.tv_register_account) as TextView
         editTextEmail = findViewById(R.id.et_email) as EditText
         editTextPassword = findViewById(R.id.et_password) as EditText
         buttonLogin = findViewById(R.id.btn_login) as Button
+        facebookButton = findViewById(R.id.facebook_login) as LoginButton
+
+        callbackManager = CallbackManager.Factory.create()
+
+        facebookButton.setReadPermissions(Arrays.asList(EMAIL))
+
+        facebookButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(result: LoginResult?) {
+                println("OMG FACEBOOK LOGIN!!!!")
+                updateUI()
+            }
+
+            override fun onCancel() {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onError(error: FacebookException?) {
+                println("OMGGGGG")
+            }
+
+        })
+
 
         makeTransparentStatusBar(true)
 
