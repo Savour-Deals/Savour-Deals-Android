836ed087a3d4be243887ddcb25813611a91e764e
wowbob396 <wowbob396@gmail.com>
2018-11-07 12:07:12 -0600

    implemented necessary functions for the vendor recycler adapter

--- app/src/main/java/com/CP/Savour/VendorRecyclerAdapter.kt
+++ app/src/main/java/com/CP/Savour/VendorRecyclerAdapter.kt
@@ -1,2 +1,27 @@
 package com.CP.Savour
 
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+class VendorRecyclerAdapter(val deals: List<Deal>, val vendors: Map<String,Vendor?>, val context: Context) : RecyclerView.Adapter<VendorRecyclerAdapter.ViewHolder>(){
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        init {
+
+        }
+    }
+}
