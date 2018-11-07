785538114fce5ded8f15e94282b1787d71d1a1a9
wowbob396 <wowbob396@gmail.com>
2018-11-07 11:54:11 -0600

    updated the dots a little bit

--- app/src/main/java/com/CP/Savour/VendorActivity.kt
+++ app/src/main/java/com/CP/Savour/VendorActivity.kt
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class VendorActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vendor)
